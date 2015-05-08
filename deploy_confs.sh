1e5675287c64dc3403bf3cf57785a40349f9e249
Kyle Jones <kjones@ip-172-31-5-180.us-west-1.compute.internal>
2015-05-08 06:39:41 +0000

    updating path for config deploy

--- deploy_confs.sh
+++ deploy_confs.sh
@@ -3,7 +3,7 @@ cd ~/
 for x in '.vimrc' '.screenrc' '.toprc' '.bashrc';
 do
     echo "Deploying $x..."
-    ln -sf "configs/$x"
+    ln -sf "repositories/configs/$x"
 done
 echo -e "\nHopefully, great success."
 echo -e "\r"
