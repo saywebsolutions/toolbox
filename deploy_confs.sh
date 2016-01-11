96e32a1529612c5feff4528f53e560c6ce2c9967
Kyle Jones <whabash090@gmail.com>
2016-01-11 08:50:06 -0800

    adding tmux confg

--- deploy_confs.sh
+++ deploy_confs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~/
-for x in '.vimrc' '.screenrc' '.toprc' '.bashrc' '.gitconfig';
+for x in '.vimrc' '.screenrc' '.toprc' '.bashrc' '.gitconfig' '.tmux.conf';
 do
     echo "Deploying $x..."
-    ln -sf "repositories/configs/$x"
+    ln -sf "repos/configs/$x"
 done
 echo -e "\nHopefully, great success."
 echo -e "\r"
