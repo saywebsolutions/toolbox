329c779883bdd41a52fef0a71bca891bd7509b5b
Kyle Jones <whabash090@gmail.com>
2017-03-12 04:01:55 -0700

    working on vps provisioning script

--- scripts/install.sh
+++ scripts/install.sh
@@ -133,6 +133,11 @@ php7.0-xml php7.0-zip php7.0-bcmath
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --filename=composer --install-dir=/usr/local/bin
+php -r "unlink('composer-setup.php');"
+
 # Misc. PHP CLI Configuration
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/cli/php.ini
@@ -149,10 +154,8 @@ id $USER
 groups $USER
 
 # Install Node.js
-curl --silent --location https://deb.nodesource.com/setup_5.x | bash -
-
+curl --silent --location https://deb.nodesource.com/setup_7.x | sudo -E bash
 apt update
-
 sudo apt-get install -y --force-yes nodejs
 
 #npm install -g pm2
@@ -161,23 +164,31 @@ sudo apt-get install -y --force-yes nodejs
 # Set The Automated Root Password
 export DEBIAN_FRONTEND=noninteractive
 
-debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $MYSQL_ROOT_PASSWORD"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $MYSQL_ROOT_PASSWORD"
-
 # Install MySQL
-apt install -y mariadb-server
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $MYSQL_ROOT_PASSWORD"
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $MYSQL_ROOT_PASSWORD"
+
+#TODO - add variable for which database to install
+
+# Install mariadb
+#export DEBIAN_FRONTEND=noninteractive
+#sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password $MYSQL_ROOT_PASSWORD'
+#sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD'
+#sudo apt-get install -y mariadb-server
+
+#apt install -y mariadb-server
 
 # Configure Password Expiration
-echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+#echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # Configure Access Permissions For Root & User
-sed -i '/^bind-address/s/bind-address.*=.*/bind-address = */' /etc/mysql/mysql.conf.d/mysqld.cnf
-mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO root@'$SERVER_IP' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-service mysql restart
+#sed -i '/^bind-address/s/bind-address.*=.*/bind-address = */' /etc/mysql/mysql.conf.d/mysqld.cnf
+#mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO root@'$SERVER_IP' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+#mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+#service mysql restart
 
 #mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$USER'@'$SERVER_IP' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 #mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$USER'@'$SERVER_IP' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
 #mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
-mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+#mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
