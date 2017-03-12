0352f7e18b38a7ea4a092f1eb2a2af2d6e4cf2f5
Kyle Jones <whabash090@gmail.com>
2017-03-12 04:20:18 -0700

    fix ssh_port variable expansion, make ssh port ufw command dynamic, do not allow root login

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,7 +26,10 @@ unzip whois htop
 
 # Disable Password Authentication Over SSH
 sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+#TODO - fix echo of $SSH_PORT
 sed -i -e '/^Port/s/^.*$/Port $SSH_PORT/' /etc/ssh/sshd_config
+sed -i "s/Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
 
 # Restart SSH
 service ssh restart
@@ -95,13 +98,12 @@ EOF
 
 # Setup UFW Firewall
 
-ufw allow 22
+ufw allow $SSH_PORT
 ufw allow 80
 ufw allow 443
 ufw --force enable
 
 # Allow FPM Restart
-
 echo "$USER ALL=NOPASSWD: /usr/sbin/service php7.0-fpm reload" > /etc/sudoers.d/php-fpm
 
 # Configure Supervisor Autostart
@@ -130,8 +132,8 @@ php7.0-mysql php7.0-mcrypt php7.0-mbstring \
 php7.0-xml php7.0-zip php7.0-bcmath
 
 # Install Composer Package Manager
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+#curl -sS https://getcomposer.org/installer | php
+#mv composer.phar /usr/local/bin/composer
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
