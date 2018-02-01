5361ab37ad6c2c517d6d70d1cfe493bc58749edb
Kyle Jones <whabash090@gmail.com>
2018-02-01 01:01:31 -0800

    pull in updates when vcs not available

---
+++ scripts/deploy-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ~/
+
+if [ ! -d "repos" ]; then
+    mkdir repos && cd repos
+    echo 'Created repos directory.'
+else
+    cd repos
+fi
+
+if [ ! -d "configs-releases" ]; then
+    mkdir configs-releases && cd configs-releases
+    echo 'Created /repos/configs-releases directory.'
+else
+    cd configs-releases
+fi
+
+wget https://github.com/whabash090/configs/archive/master.zip
+
+DIRNAME=configs-$(date +%Y%m%d%H%M%S)
+
+unzip master.zip -d $DIRNAME
+
+rm master.zip
+
+cd ../
+
+if [ ! -d "configs/.git" ]; then
+  ln -sf $DIRNAME configs
+else
+  echo 'VCS detected, aborting update.'
+fi
