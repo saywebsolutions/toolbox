00a0fce4e671b606087fc9cdb2b0e607278f5674
Kyle <whabash090@gmail.com>
2019-04-06 19:32:14 -0700

    ubuntu 18.04 spotify install script

---
+++ scripts/ubuntu-18-04/install-spotify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# directions: https://www.spotify.com/us/download/linux/
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client -y
+
+echo "################################################################"
+echo "###################   spotify installed   ######################"
+echo "################################################################"
