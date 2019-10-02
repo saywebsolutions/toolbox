0abe4f21e7f747c3fb231838179100732e676cf0
Kyle <kyle@saywebsolutions.com>
2019-10-02 16:16:20 -0700

    add use statement before flushing privileges

--- scripts/db.sh
+++ scripts/db.sh
@@ -4,8 +4,9 @@ DB_NAME=""
 DB_USER="${DB_NAME}_usr"
 DB_PASS=""
 
-SQL="DROP USER IF EXISTS ${DB_USER};CREATE DATABASE IF NOT EXISTS ${DB_NAME};GRANT ALL ON ${DB_NAME} TO '${DB_USER}'@'*' IDENTIFIED BY '${DB_PASS}';FLUSH PRIVILEGES;"
+SQL="DROP USER IF EXISTS ${DB_USER};CREATE DATABASE IF NOT EXISTS ${DB_NAME};GRANT ALL ON ${DB_NAME} TO '${DB_USER}'@'*' IDENTIFIED BY '${DB_PASS}';USE ${DB_NAME};FLUSH PRIVILEGES;"
 
 echo "Executing: ${SQL}\n"
 
-sudo mysql -u root -e "${SQL}"
+#set mysql password with env variable prior to running command: export MYSQL_PWD=
+mysql -u root -ve "${SQL}"
