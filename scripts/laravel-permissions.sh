746e1f2281755c7a1f3d012e8f9cbe02d16469fa
Kyle <kyle@saywebsolutions.com>
2020-02-04 12:20:44 -0800

    script to set laravel project permissions

--- scripts/laravel-permissions.sh
+++ scripts/laravel-permissions.sh
@@ -5,7 +5,7 @@ U=$(id -u -n) # <-- posix current user
 
 echo ${U}
 
-chown -R ${U}:www-data ../
-find ../ -type d -exec chmod 2750 {} \+
-find ../ -type f -exec chmod 640 {} \+
-chmod -R ug+rwx storage bootstrap/cache vendor/bin/phpunit
+sudo chown -R ${U}:www-data ../
+sudo find ../ -type d -exec chmod 2750 {} \+
+sudo find ../ -type f -exec chmod 640 {} \+
+sudo chmod -R ug+rwx storage bootstrap/cache vendor/bin/phpunit
