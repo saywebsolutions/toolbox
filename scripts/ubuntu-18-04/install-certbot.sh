6308a2136ca4f93c9f1fa41fbcaabb84e2d7c58f
Kyle <kyle@saywebsolutions.com>
2020-02-04 15:37:12 -0800

    certbot install on ubuntu 18.04

---
+++ scripts/ubuntu-18-04/install-certbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
